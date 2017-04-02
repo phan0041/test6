b66c5c6ce6ffba602a18e38bc3762f45eb846165
Dan <vandan193@gmail.com>
2017-04-02 19:05:23 +0800

    a

--- ExamPapers/settings.py
+++ ExamPapers/settings.py
@@ -15,7 +15,6 @@ DAJAXICE_MEDIA_PREFIX="dajaxice"
 #online database
 import dj_database_url
 
-DATABASES[‘default’] = dj_database_url.config()
 DATABASES = {
     # staging database
     'default': {
