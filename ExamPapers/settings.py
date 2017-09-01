35096b72ebf66241e7637bca4dd67dd5779864ac
Dan <vandan193@gmail.com>
2017-09-01 16:17:14 +0800

    change db credentials

--- ExamPapers/settings.py
+++ ExamPapers/settings.py
@@ -20,10 +20,10 @@ DATABASES = {
     # staging database
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'd23ogluv8012a5',                      # Or path to database file if using sqlite3.
-        'USER': 'slzecnmkgrzllu',                      # Not used with sqlite3.
-        'PASSWORD': 'c83e5534a7e21a98d0545db63a58190f6c96fcf3779f8f72dd0640959246e6ff',                  # Not used with sqlite3.
-        'HOST': 'ec2-54-225-67-3.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'djjh2k4dmedvh',                      # Or path to database file if using sqlite3.
+        'USER': 'zoyblkcbocnacz',                      # Not used with sqlite3.
+        'PASSWORD': 'cb44a85ca11fbace8febfa66972fafd5018920aacd6653127983f63b95d2638e',                  # Not used with sqlite3.
+        'HOST': 'ec2-174-129-218-106.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     },
     # dev db
